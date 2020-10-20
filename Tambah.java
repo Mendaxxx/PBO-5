7fff554555145e8ef19aff4722ab04d52dcbe7ba
Mendaxxx <35775429+Mendaxxx@users.noreply.github.com>
2020-10-20 21:04:48 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,5 @@
+package com.pboreg;
+
+public class Tambah {
+
+}
---
+++ classss.java
@@ -0,0 +1,14 @@
+package com.pboreg;
+
+class classss {
+
+    public int x = 5;
+    int y = 5;
+    private int z = 20;
+
+    // method
+    public int showZ(){
+        return  this.z;
+    }
+
+}
